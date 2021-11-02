88e0149375124cc148a89b0deda194a971aea9d8
christian-bromann <mail@christian-bromann.com>
2021-11-03 00:37:12 +0100

    also store fonts

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,8 @@
 module.exports = {
 	globDirectory: 'public/',
 	globPatterns: [
-		'**/*.{html,xml,css,jpeg,png,ico,svg,jpg,js,json,txt,webmanifest}'
+		'**/*.{html,xml,css,jpeg,png,ico,svg,jpg,js,json,txt,woff2,webmanifest}',
+        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css'
 	],
 	ignoreURLParametersMatching: [
 		/^utm_/,
