52726ed7f9c4c1d733fbcd6859df59c94f04f7d4
christian-bromann <mail@christian-bromann.com>
2021-11-05 16:49:13 +0100

    include woff in sw patterns

--- workbox-config.js
+++ workbox-config.js
@@ -1,8 +1,7 @@
 module.exports = {
 	globDirectory: 'public/',
 	globPatterns: [
-		'**/*.{html,xml,css,jpeg,png,ico,svg,jpg,js,json,txt,woff2,webmanifest}',
-        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css'
+		'**/*.{html,xml,css,jpeg,png,ico,svg,jpg,js,json,txt,woff,woff2,webmanifest}'
 	],
 	ignoreURLParametersMatching: [
 		/^utm_/,
